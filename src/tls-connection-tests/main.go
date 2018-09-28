e46d789a18096efa9c47001640657a034da0231e
Caroline Taymor <ctaymor@pivotal.io>
2018-09-28 11:50:54 -0700

    Errand creates 300 sleep queries in parallel goroutinues
    This allows us to explore how the connection pooling is working with the
    go-sql-driver
    
    [#160806738]
    
    Signed-off-by: Chris Tarazi <ctarazi@pivotal.io>

--- src/tls-connection-tests/main.go
+++ src/tls-connection-tests/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Print("It works!")
+	fmt.Println("It works!")
+
+	db, err := sql.Open("mysql", "diego:1dcoqrm6gpuoyqzr7fms@tcp(sql-db.service.cf.internal:3306)/diego")
+	if err != nil {
+		fmt.Println("Failed to open db connection.")
+		os.Exit(1)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Can't ping the db.")
+		os.Exit(1)
+	}
+	fmt.Println("Was able to ping the db.")
+
+	defer db.Close()
+	db.SetMaxOpenConns(300)
+	db.SetMaxIdleConns(300)
+
+	var wg sync.WaitGroup
+	goroutines := 300
+	wg.Add(goroutines)
+
+	cmd := "SELECT SLEEP(?);"
+	f := func() {
+		defer wg.Done()
+
+		res, err := db.Exec(cmd, 60)
+		fmt.Println("Doing a new exec")
+		if err != nil {
+			fmt.Printf("Failed to exec '%s'.\n", cmd)
+			os.Exit(1)
+		}
+
+		rows, _ := res.RowsAffected()
+		fmt.Printf("Rows affected '%d'.\n", rows)
+	}
+
+	for i := 0; i < goroutines; i++ {
+		go f()
+	}
+
+	fmt.Printf("Spun off %d goroutines.\n", goroutines)
+	wg.Wait()
+	fmt.Println("Done waiting for goroutines.")
 }
